c7989f1ee766c9f3eefe649b526f9b704e3031ea
yoanntouzain <ytouzain@gmail.com>
2022-09-06 17:46:09 +0200

    correction du responsive sur la page error

--- kasa/src/pages/Error/Error.jsx
+++ kasa/src/pages/Error/Error.jsx
@@ -3,16 +3,18 @@ import './error.css'
 
 export default function Error() {
   return (
-    <div>
+    <div className="main">
       <div className="container-error">
         <div className="container-title">
           <h1 className="title-error">404</h1>
         </div>
+
         <div className="container-content">
           <p className="content-error">
             Oups! La page que vous demandez n'existe pas.
           </p>
         </div>
+
         <div className="container-link">
           <Link to="/">Retourner sur la page d’accueil</Link>
         </div>
